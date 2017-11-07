18c8aa8ece17b95d32c8ca7dcd74445471f1e011
Thomas Ducrot <td@reppa.net>
2017-11-07 16:34:15 +0100

    tested script for deployment

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,16 @@ executableName='goSafeChatBackend'
 newExecutableName="new_$executableName"
 oldExecutableName="old_$executableName"
 
+export GOPATH=$(pwd)
+
 echo 'Compiling the project locally'
 ./build.sh
 chmod +x $executableName
 
 echo "Uploading the new executable $newExecutableName"
-scp -i ~/.ssh/pakpak.pem goSafeChatBackend ubuntu@ec2-35-165-24-103.us-west-2.compute.amazonaws.com:~/$newExecutableName
+mv $executableName $newExecutableName
+scp -i ~/.ssh/pakpak.pem $newExecutableName Dockerfile ubuntu@ec2-35-165-24-103.us-west-2.compute.amazonaws.com:~
+mv $newExecutableName $executableName
 
 ssh -i ~/.ssh/pakpak.pem ubuntu@ec2-35-165-24-103.us-west-2.compute.amazonaws.com << EOF
 set +e
@@ -21,8 +25,8 @@ if [ -f $executable ]; then
 fi
 mv $newExecutableName $executable
 
-docker rm -f safechat_docker
-docker build -t safechat .
-docker run --name="safechat_docker" -d -t safechat
+sudo docker rm -f safechat_docker
+sudo docker build -t safechat .
+sudo docker run --name="safechat_docker" -d -t safechat
 
 EOF
